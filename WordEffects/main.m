9176d102a1beac8d0f10b07a8f70c085b18e6332
Alex Robertson <hello_im_alex@hotmail.com>
2015-10-19 16:54:06 -0700

    initial commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Alex on 2015-10-19.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
